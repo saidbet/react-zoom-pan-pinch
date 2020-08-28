b7e652df3ed37594b62adef36e435df089f624fc
Said Betmurzaev <said.b@outlook.com>
2020-08-28 15:09:42 +0200

    remove dist from gitignore

---
+++ dist/components/TransformWrapper.d.ts
@@ -0,0 +1,3 @@
+import { PropsList } from "../store/interfaces/propsInterface";
+declare const TransformWrapper: ({ children, defaultPositionX, defaultPositionY, defaultScale, onWheelStart, onWheel, onWheelStop, onPanningStart, onPanning, onPanningStop, onPinchingStart, onPinching, onPinchingStop, onZoomChange, onAnimationStop, ...rest }: PropsList) => JSX.Element;
+export { TransformWrapper };
---
+++ dist/store/velocity/index.d.ts
@@ -0,0 +1,3 @@
+export declare function handleFireVelocity(): void;
+export declare function animateVelocity(callback: any): any;
+export declare function calculateVelocityStart(event: any): void;
